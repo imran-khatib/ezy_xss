5acf309ef1ea5c8328a7218afdfa7ae120640c7a
e1pr0f3ss0r <imrankhatib99@gmail.com>
2022-07-29 00:49:29 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+green=`echo -en "\e[32m"`
+blue=`echo -en "\e[34m"`
+NC='\033[0m'
+
+
+echo -e "${green}[*] Installing Essentials${NC}"
+apt-get update -y -silent
+apt-get upgrade -y -silent
+cd $HOME
+mkdir tools
+echo "${blue} [*] Installing Parampsider..... ${NC}"
+cd  ~/tools
+git clone https://github.com/devanshbatham/ParamSpider.git
+cd ParamSpider
+pip3 install -r requirements.txt
+cd .. 
+echo "${blue}[*] Installing Dalfox..... ${NC}"
+cd  ~/tools
+go install -v github.com/hahwul/dalfox/v2@latest
+
+cd ..
+echo "${blue}[*] Installing qsreplace..... ${NC}"
+cd  ~/tools
+go install -v github.com/tomnomnom/qsreplace@latest
+
