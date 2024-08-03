c0ea50d43af4b7cfe01dbbe48876004a07d0cd6d
e1pr0f3ss0r <imrankhatib99@gmail.com>
2024-08-03 14:50:46 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -1,42 +1,52 @@
 #!/bin/bash
 
-green=`echo -en "\e[32m"`
-blue=`echo -en "\e[34m"`
+# Color codes
+green=$(echo -en "\e[32m")
+blue=$(echo -en "\e[34m")
 NC='\033[0m'
-TOOLS=( "dalfox"
-        "qsreplace"
-        "python3 paramspider.py"
-        "waybackurls"
-      )
+
+# List of tools to be installed
+TOOLS=(
+    "dalfox"
+    "qsreplace"
+    "python3 paramspider.py"
+    "waybackurls"
+)
+
 echo -e "${green}[*] Installing Essentials${NC}"
+
+# Update and upgrade packages silently
 apt-get update -y --silent 
-apt-get upgrade -y -silent
+apt-get upgrade -y --silent
 
-mkdir ~/tools
+# Create tools directory
+mkdir -p ~/tools
 
+# Install required tools
 for tool in "${TOOLS[@]}"; do
-    if ! command -v "$tool" &> /dev/null; then
+    if ! command -v "${tool%% *}" &> /dev/null; then
         echo "$tool is not installed. Installing..."
+        
         if [ "$tool" = "dalfox" ]; then
             GO111MODULE=on go install github.com/hahwul/dalfox/v2@latest
-        elif [ "$tool" = "paramspider" ]; then
-            echo "${blue} [*] Installing Parampsider..... ${NC}"
-            cd  ~/tools
+        elif [ "$tool" = "python3 paramspider.py" ]; then
+            echo -e "${blue}[*] Installing ParamSpider...${NC}"
+            cd ~/tools
             git clone https://github.com/devanshbatham/ParamSpider.git
             cd ParamSpider
             pip3 install -r requirements.txt
-            cd .. 
-             echo "${green}Installed Parampsider..${NC}"
+            cd ..
+            echo -e "${green}Installed ParamSpider.${NC}"
         elif [ "$tool" = "waybackurls" ]; then
-             echo "${green}Installing Waybackurls..${NC}"
-             GO111MODULE=on go install github.com/tomnomnom/waybackurls@latest
-             echo "${green}Installed Waybackurls..${NC}"
-        elif [ "$tool" = "qsreplace" ]; then  
-             echo "${green}Installing qsrepace..${NC}"
-             GO111MODULE=on go install github.com/tomnomnom/qsreplace@latest
-             echo "${green}Installed qsrepace${NC}"
+            echo -e "${green}Installing Waybackurls...${NC}"
+            GO111MODULE=on go install github.com/tomnomnom/waybackurls@latest
+            echo -e "${green}Installed Waybackurls.${NC}"
+        elif [ "$tool" = "qsreplace" ]; then
+            echo -e "${green}Installing qsreplace...${NC}"
+            GO111MODULE=on go install github.com/tomnomnom/qsreplace@latest
+            echo -e "${green}Installed qsreplace.${NC}"
         fi
-        else
+    else
         echo "$tool is already installed"
-        fi
+    fi
 done
